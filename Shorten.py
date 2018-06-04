88fcdec92bef46d113a6791caad3eb3de61e639a
Henry Qin <hq6@cs.stanford.edu>
2018-06-04 00:49:02 -0700

    Add a control socket for internal use.
    Eventually this will be used for deletion of entries, but it could also be used as a convenient way to add and remove entries from the URL Map.
    Note that this socket should always be firewall-protected.

--- Shorten.py
+++ Shorten.py
@@ -5,6 +5,16 @@ from signal import signal, SIGINT, SIGTERM
 from sys import exit
 import logging
 
+# Used for control interface
+from threading import Thread
+import socket, sys
+
+# Control-related variables
+CONTROL_PORT = 7770
+CONTROL_HOST = '127.0.0.1'
+MAX_COMMAND_LENGTH = 4096
+shutdownRequested = False
+
 PORT=8880
 FORM_PATH="Shorten.html"
 
@@ -87,17 +97,68 @@ def run(server_class=BaseHTTPServer.HTTPServer,
     httpd = server_class(server_address, handler_class)
     httpd.serve_forever()
 
+def startControlInterface():
+    def handle(clientsocket):
+        print "Client connected to control interface..."
+        clientsocket.settimeout(1)
+        clientsocket.send(">")
+        promptSent = True
+        global shutdownRequested
+        while not shutdownRequested:
+          try:
+            if not promptSent:
+              clientsocket.send(">")
+              promptSent = True
+            buf = clientsocket.recv(MAX_COMMAND_LENGTH)
+            buf = buf.strip()
+            if buf == 'exit':
+                print "Client disconnected from control interface..."
+                clientsocket.close()
+                return
+            else:
+                clientsocket.send("Unrecognized command '%s'\n" % buf)
+            promptSent = False
+          except socket.timeout:
+            pass
+    def accept_commands():
+        global shutdownRequested
+        serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        serversocket.bind((CONTROL_HOST, CONTROL_PORT))
+        serversocket.listen(10)
+
+        # This is necessary to allow clean shutdowns
+        serversocket.settimeout(1)
+        while not shutdownRequested:
+          try:
+            (clientsocket, address) = serversocket.accept()
+            handler_thread = Thread(target=handle, args=(clientsocket,))
+            handler_thread.start()
+          except socket.timeout:
+            pass
+
+    thread = Thread(target=accept_commands)
+    thread.start()
+
 def main():
     # Set up signal handlers
     def handler(signum, frame):
+        global shutdownRequested
+        shutdownRequested = True
         print "Exiting due to signal %d." % signum
         exit(0)
+
     signal(SIGINT, handler)
     signal(SIGTERM, handler)
 
 	# Set up logging
     logging.basicConfig(level=logging.INFO)
 
+    # Set up a control interface on a separate thread that we can telnet to and
+    # issue commands.
+    startControlInterface()
+
+    # Start the web server
     run(BaseHTTPServer.HTTPServer, ShortenURLHandler);
 
 if __name__ == "__main__":
